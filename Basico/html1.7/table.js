de43243eb8e15740a74fdb97aaf35117da2c1143
italo <italomauricio98@gmail.com>
2023-08-26 18:46:34 -0300

    refatorando o js e consertando o css

--- Basico/html1.7/table.js
+++ Basico/html1.7/table.js
@@ -1,64 +1,69 @@
 function createTable(items, propertyNames, divId) {
     const div = document.getElementById(divId);
-    const headers = propertyNames.map(propertyName => propertyName.charAt(0).toUpperCase() + propertyName.slice(1));
+    const headers = propertyNames.map(propertyName => capitalizeFirstLetter(propertyName));
     const headerRow = `<tr>${headers.map(header => `<th>${header}</th>`).join("")}</tr>`;
-
+    
     const itemsHtml = items.map(item => {
-        const cells = propertyNames.map(propertyName => {
-            const value = item[propertyName] || "";
-            return `<td>${value}</td>`;
-        });
-        return `<tr>${cells.join("")}</tr>`;
+      const cells = propertyNames.map(propertyName => `<td>${item[propertyName] || ""}</td>`);
+      return `<tr>${cells.join("")}</tr>`;
     });
-
+  
     div.innerHTML = `<table>${headerRow}${itemsHtml.join("\n")}</table>`;
-}
-
-async function fetchData(url = "https://random-data-api.com/api/v2/beers?size=3") {
+  }
+  
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  async function fetchData(url = "https://random-data-api.com/api/v2/beers?size=3") {
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
     } catch (error) {
-        console.error("Error fetching data:", error);
-        throw error;
+      console.error("Error fetching data:", error);
+      throw error;
     }
-}
-
-async function loadBeers() {
+  }
+  
+  async function loadBeers() {
     try {
-        const cervejas = await fetchData();
-        createTable(cervejas, ["name", "alcohol", "style", "ibu", "brand"], "cervejasDiv");
+      const cervejas = await fetchData();
+      renderBeerTable(cervejas);
     } catch (error) {
-        document.getElementById("cervejasDiv").innerHTML = "Fudeu papae...";
+      displayErrorMessage();
     }
-}
-
-async function sortBeers() {
+  }
+  
+  async function sortBeers() {
     try {
-        const cervejas = await fetchData();
-        cervejas.sort((a, b) => a.name.localeCompare(b.name));
-        createTable(cervejas, ["name", "alcohol", "style", "ibu", "brand"], "cervejasDiv");
+      const cervejas = await fetchData();
+      cervejas.sort((a, b) => a.name.localeCompare(b.name));
+      renderBeerTable(cervejas);
     } catch (error) {
-        document.getElementById("cervejasDiv").innerHTML = "Vixe...";
+      displayErrorMessage();
     }
-}
-
-async function shuffleBeers() {
+  }
+  
+  async function shuffleBeers() {
     try {
-        const cervejas = await fetchData();
-        const shuffledCervejas = [...cervejas].sort(() => Math.random() - 0.5); // Fisher-Yates shuffle
-        createTable(shuffledCervejas, ["name", "alcohol", "style", "ibu", "brand"], "cervejasDiv");
+      const cervejas = await fetchData();
+      const shuffledCervejas = [...cervejas].sort(() => Math.random() - 0.5); // Fisher-Yates shuffle
+      renderBeerTable(shuffledCervejas);
     } catch (error) {
-        document.getElementById("cervejasDiv").innerHTML = "Vixe...";
+      displayErrorMessage();
     }
-}
-
-const botao = document.getElementById("load");
-botao.addEventListener("click", loadBeers);
-
-const botaoOrdenar = document.getElementById("sort");
-botaoOrdenar.addEventListener("click", sortBeers);
-
-const botaoEmbaralhar = document.getElementById("shuffle");
-botaoEmbaralhar.addEventListener("click", shuffleBeers);
+  }
+  
+  function renderBeerTable(cervejas) {
+    createTable(cervejas, ["name", "alcohol", "style", "ibu", "brand"], "cervejasDiv");
+  }
+  
+  function displayErrorMessage() {
+    document.getElementById("cervejasDiv").innerHTML = "Ocorreu um erro ao carregar os dados.";
+  }
+  
+  document.getElementById("load").addEventListener("click", loadBeers);
+  document.getElementById("sort").addEventListener("click", sortBeers);
+  document.getElementById("shuffle").addEventListener("click", shuffleBeers);
+  
