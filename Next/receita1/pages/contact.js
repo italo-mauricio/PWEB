246d76c0dc455b3e1d661d94615eaf666fd8f64e
italo <italomauricio98@gmail.com>
2023-11-12 21:58:47 -0300

    update

--- Next/receita1/pages/contact.js
+++ Next/receita1/pages/contact.js
@@ -4,7 +4,7 @@ import styles from '../style/contact.module.css';
 function Contact() {
     return (
         <div className={styles.contact}>
-            <h2>Contos</h2>
+            <h2>Contatos</h2>
             <p>Caso queira conhecer mais sobre a festa:</p>
             <ul>
                 <li><a href="https://www.instagram.com/festadesantanacaicooficial/?hl=pt" target="_blank" rel="noopener noreferrer">Instagram</a></li>
