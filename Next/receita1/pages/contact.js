9901357ba70ed95fd02ed5a43be184c6bfc2fa3d
italo <italomauricio98@gmail.com>
2023-11-13 00:31:01 -0300

    update

--- Next/receita1/pages/contact.js
+++ Next/receita1/pages/contact.js
@@ -1,5 +1,7 @@
+// Importe a imagem no topo do arquivo
 import React from 'react';
 import styles from '../style/contact.module.css';
+import instagramLogo from '../public/images/1.png'; // Remova "public" do caminho
 
 function Contact() {
     return (
@@ -7,7 +9,12 @@ function Contact() {
             <h2>Contatos</h2>
             <p>Caso queira conhecer mais sobre a festa:</p>
             <ul>
-                <li><a href="https://www.instagram.com/festadesantanacaicooficial/?hl=pt" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                <li>
+                    <a href="https://www.instagram.com/festadesantanacaicooficial/?hl=pt" target="_blank" rel="noopener noreferrer">
+                        <img src={instagramLogo} alt="" className={styles.instagramLogo} />
+                        Instagram
+                    </a>
+                </li>
             </ul>
         </div>
     );
