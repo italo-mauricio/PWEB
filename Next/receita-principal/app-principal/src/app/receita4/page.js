09d07bffbfc9402b3fe9f17c15307031f5afbf01
italo <italomauricio98@gmail.com>
2023-11-30 00:18:29 -0300

    update

--- Next/receita-principal/app-principal/src/app/receita4/page.js
+++ Next/receita-principal/app-principal/src/app/receita4/page.js
@@ -4,7 +4,7 @@ import useSWR from "swr"
 
 export default function Movies2() {
   const { data, error } = useSWR(
-    `http://www.omdbapi.com/?i=tt3896198&apikey=2517b62d&s=bagdad`,
+    `https://www.omdbapi.com/?i=tt3896198&apikey=2517b62d&s=bagdad`,
     fetcher
   )
 
