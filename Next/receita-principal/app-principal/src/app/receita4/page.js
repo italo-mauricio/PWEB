adca1f95161d3c2584dcce88c98a4ce184ae69d7
italo <italomauricio98@gmail.com>
2023-11-30 00:26:51 -0300

    deixando a receita 4 em uma grid

--- Next/receita-principal/app-principal/src/app/receita4/page.js
+++ Next/receita-principal/app-principal/src/app/receita4/page.js
@@ -1,34 +1,38 @@
-"use client"; 
-import Link from "next/link"
-import useSWR from "swr"
+"use client";
+import Link from "next/link";
+import useSWR from "swr";
 
 export default function Movies2() {
   const { data, error } = useSWR(
     `https://www.omdbapi.com/?i=tt3896198&apikey=2517b62d&s=bagdad`,
     fetcher
-  )
+  );
 
-  if (error) return <div>Falha na requisição...</div>
-  if (!data) return <div>Carregando...</div>
+  if (error) return <div>Falha na requisição...</div>;
+  if (!data) return <div>Carregando...</div>;
 
   return (
-    <div>
-      {data.Search.map((m) => (
-        <div key={m.imdbID}>
-          <Link href={`/receita4/${m.imdbID}`}>
-            <img src={m.Poster} alt={m.Title} />
-            <h2>{m.Title}</h2>
-          </Link>
-        </div>
-      ))}
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {data.Search.map((m) => (
+          <div key={m.imdbID}>
+            <Link href={`/receita4/${m.imdbID}`}>
+              <div className="text-center">
+                <img src={m.Poster} alt={m.Title} className="mb-2" />
+                <h2 className="text-lg">{m.Title}</h2>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 async function fetcher(url) {
-  const res = await fetch(url)
-  const json = await res.json()
-  return json
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
 }
 
 
